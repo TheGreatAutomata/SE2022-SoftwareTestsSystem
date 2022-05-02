96f5641a52077410331fd6e92ed2bd764512c7c2
TheGreatAutomata <3368925785@qq.com>
2022-05-01 23:07:38 -0700

    first

---
+++ gateway-server/src/test/java/com/micro/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/microserivces/njutestsproject/NjuTestsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserivces.njutestsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjuTestsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
