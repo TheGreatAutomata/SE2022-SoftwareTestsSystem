b43590245335cbe8971a9f9037585ea3c4b02b50
TheGreatAutomata <3368925785@qq.com>
2022-05-14 02:31:29 -0700

    minio try

--- delegation-server/src/main/java/com/micro/delegationserver/rest/delegationController.java
+++ delegation-server/src/main/java/com/micro/delegationserver/rest/delegationController.java
@@ -4,14 +4,21 @@ import com.micro.delegationserver.mapper.CreatDelegationRequestMapper;
 import com.micro.delegationserver.model.CreatDelegationRequest;
 import com.micro.delegationserver.model.Delegation;
 import com.micro.delegationserver.service.DelegationService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import com.micro.api.DelegationApi;
 import com.micro.dto.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.net.URI;
 import java.util.HashMap;
@@ -69,6 +76,16 @@ public class delegationController implements DelegationApi{
 
         return ResponseEntity.ok(usrId);
     }
-
-
+    @Override
+    public ResponseEntity<Void> createDelegationFile(String id, String usrName, String usrId, String usrRole, List<MultipartFile> file) {
+        for (MultipartFile f : file)
+        {
+            if(!f.isEmpty())
+            {
+                System.out.println("**TheFileName: "+ f.getOriginalFilename());
+                System.out.println(("**TheFileSize: "+f.getSize()));
+            }
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
 }
