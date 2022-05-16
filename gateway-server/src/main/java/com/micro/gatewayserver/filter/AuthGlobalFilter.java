0085edea2c3704c0ba19e7acbd5b18acc8e8cffd
runbanglu <2320470008@qq.com>
2022-05-16 01:31:09 -0700

    can route and filter

--- gateway-server/src/main/java/com/micro/gatewayserver/filter/AuthGlobalFilter.java
+++ gateway-server/src/main/java/com/micro/gatewayserver/filter/AuthGlobalFilter.java
@@ -1,15 +1,22 @@
 package com.micro.gatewayserver.filter;
+import com.micro.gatewayserver.security.JwtUtils;
 import com.nimbusds.jose.JWSObject;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import com.micro.gatewayserver.temp.TempBodyForLogin;
@@ -19,31 +26,79 @@ import java.util.List;
 import java.util.Objects;
 
 @Component
-public class AuthGlobalFilter implements GlobalFilter, Ordered
+public class AuthGlobalFilter implements GlobalFilter
 {
-    @Override
-    public int getOrder()
-    {
-        return 0;
-    }
+    @Autowired
+    private RouterValidator routerValidator;//custom route validator
+    @Autowired
+    private JwtUtils jwtUtils;
+
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
+//    {
+//        ServerHttpRequest serverHttpRequest = exchange.getRequest();
+//        String token = serverHttpRequest.getHeaders().getFirst("token");
+////        Flux<DataBuffer> body = serverHttpRequest.getBody();
+////        StringBuilder sb = new StringBuilder();
+////        body.subscribe(buffer -> {
+////            byte[] bytes = new byte[buffer.readableByteCount()];
+////            buffer.read(bytes);
+////            DataBufferUtils.release(buffer);
+////            String bodyString = new String(bytes, StandardCharsets.UTF_8);
+////            sb.append(bodyString);
+////        });
+////        //check
+////        System.out.println(sb.toString());
+//        //
+////        JSONObject jsonObject=new JSONObject();
+////        jsonObject.put("usrName","root usr");
+////        jsonObject.put("usrId","root usr");
+////        jsonObject.put("authorities","root usr");
+////        String base64 = Base64.getEncoder().encodeToString(jsonObject.toJSONString().getBytes(StandardCharsets.UTF_8));
+//        ServerHttpRequest request = exchange.getRequest().mutate().header("usrName", "rootUsr").build();
+//        request = exchange.getRequest().mutate().header("usrId", "rootUsr").build();
+//        request = exchange.getRequest().mutate().header("usrRole", "rootUsr").build();
+//        exchange  = exchange.mutate().request(request).build();
+//        return chain.filter(exchange);
+//    }
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain)
-    {
-        ServerHttpRequest serverHttpRequest = exchange.getRequest();
-        String token = serverHttpRequest.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-        //check
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
 
-        //
-//        JSONObject jsonObject=new JSONObject();
-//        jsonObject.put("usrName","root usr");
-//        jsonObject.put("usrId","root usr");
-//        jsonObject.put("authorities","root usr");
-//        String base64 = Base64.getEncoder().encodeToString(jsonObject.toJSONString().getBytes(StandardCharsets.UTF_8));
-        ServerHttpRequest request = exchange.getRequest().mutate().header("usrName", "rootUsr").build();
-        request = exchange.getRequest().mutate().header("usrId", "rootUsr").build();
-        request = exchange.getRequest().mutate().header("usrRole", "rootUsr").build();
-        exchange  = exchange.mutate().request(request).build();
+        if (routerValidator.isSecured.test(request)) {
+            if (this.isAuthMissing(request))
+                return this.onError(exchange, "Authorization header is missing in request", HttpStatus.UNAUTHORIZED);
+
+            final String token = this.getAuthHeader(request);
+
+            if (!jwtUtils.validateJwtToken(token))
+                return this.onError(exchange, "Authorization header is invalid", HttpStatus.UNAUTHORIZED);
+
+            this.populateRequestWithHeaders(exchange, token);
+        }
         return chain.filter(exchange);
     }
+
+    /*PRIVATE*/
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    private String getAuthHeader(ServerHttpRequest request) {
+        return request.getHeaders().getOrEmpty("Authorization").get(0);
+    }
+
+    private boolean isAuthMissing(ServerHttpRequest request) {
+        return !request.getHeaders().containsKey("Authorization");
+    }
+
+    private void populateRequestWithHeaders(ServerWebExchange exchange, String token) {
+        exchange.getRequest().mutate()
+                .build();
+    }
 }
+
--- gateway-server/src/main/java/com/micro/gatewayserver/security/JwtUtils.java
+++ gateway-server/src/main/java/com/micro/gatewayserver/security/JwtUtils.java
@@ -1,7 +1,7 @@
 package com.micro.gatewayserver.security;
 
+import java.security.Key;
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
