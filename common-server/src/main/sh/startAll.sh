ba1c42c564263589811c23dc8cbb6a1fd95b7cb7
TheGreatAutomata <3368925785@qq.com>
2022-07-02 05:05:12 -0700

    max

--- common-server/src/main/sh/startAll.sh
+++ common-server/src/main/sh/startAll.sh
@@ -4,10 +4,7 @@ LOGDIR="/root/deployment"
 
 NOW="discovery-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
-sleep 10
-
-NOW="gateway-server"
-nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
+sleep 8
 
 NOW="delegation-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT-exec.jar > ${LOGDIR}/${NOW}.txt &
@@ -22,4 +19,9 @@ NOW="test-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
 
 NOW="spring-boot-security-jwt"
+nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
+
+sleep 15
+
+NOW="gateway-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
