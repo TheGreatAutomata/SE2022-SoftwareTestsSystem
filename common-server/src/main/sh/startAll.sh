b32ecb0c019e95fe06a0034e585fbd9ffe6b60bf
TheGreatAutomata <3368925785@qq.com>
2022-07-01 23:24:08 -0700

    jenkins

--- common-server/src/main/sh/startAll.sh
+++ common-server/src/main/sh/startAll.sh
@@ -3,9 +3,9 @@
 LOGDIR="/root/deployment"
 
 NOW="discovery-server"
-nohup java -jar "${WORKSPACE}/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar" > "${LOGDIR}/${NOW}.txt" &
+nohup java -jar "${WORKSPACE}/SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar" > "${LOGDIR}/${NOW}.txt" &
 sleep 10
 
 NOW="gateway-server"
-nohup java -jar "${WORKSPACE}/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar" > "${LOGDIR}/${NOW}.txt" &
+nohup java -jar "${WORKSPACE}/SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar" > "${LOGDIR}/${NOW}.txt" &
 
