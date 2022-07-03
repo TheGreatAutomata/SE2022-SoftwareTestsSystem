3cf3077971ceecf304684298e3d50f3cb8da491b
Australcnt <924690736@qq.com>
2022-07-03 16:20:35 +0800

    try last dock with new deployment

--- common-server/src/main/sh/startAll.sh
+++ common-server/src/main/sh/startAll.sh
@@ -10,7 +10,7 @@ NOW="delegation-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT-exec.jar > ${LOGDIR}/${NOW}.txt &
 
 NOW="contract-server"
-nohup java -jar ./deployment/${NOW}-0.0.1-SNAPSHOT-exec.jar > ${LOGDIR}/${NOW}.txt &
+nohup java -jar ./SE2022-SoftwareTestsSystem/deployment/${NOW}-0.0.1-SNAPSHOT-exec.jar > ${LOGDIR}/${NOW}.txt &
 
 NOW="sample-server"
 nohup java -jar ./SE2022-SoftwareTestsSystem/${NOW}/target/${NOW}-0.0.1-SNAPSHOT.jar > ${LOGDIR}/${NOW}.txt &
